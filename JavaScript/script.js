777cb6da4b00ce8c6f35e0731c7541730d9a9854
Gabriel Lentine <gabrielmarinholentine@gmail.com>
2025-05-02 22:29:57 -0300

    Carrosel nas fotos do consultório finalizado.

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -17,6 +17,31 @@ document.querySelectorAll(".perguntas").forEach((pergunta) => {
 // efeito nos depoimentos
 
 // efeito carrosel no consultorio
+let nextbutton = document.getElementById("next");
+let prevbutton = document.getElementById("prev");
+let posts = document.querySelectorAll(".post");
+let ativo = 0;
+let primeirapossicao = 0;
+let ultimapossicao = posts.length - 1;
+
+function mostrarPost(index) {
+  posts.forEach((post, i) => {
+    post.classList.remove("ativo");
+    if (i === index) {
+      post.classList.add("ativo");
+    }
+  });
+}
+
+nextbutton.onclick = () => {
+  ativo = ativo + 1 > ultimapossicao ? primeirapossicao : ativo + 1;
+  mostrarPost(ativo);
+};
+
+prevbutton.onclick = () => {
+  ativo = ativo - 1 < primeirapossicao ? ultimapossicao : ativo - 1;
+  mostrarPost(ativo);
+};
 
 // accordion para o faq
 const questions = document.querySelectorAll(".faq-pergunta");
